506db60eea6b1eb86148242bb3f09373f94233c7
Nastassia Hryshchuk <19anastasia86@gmail.com>
2018-01-07 21:56:04 +0300

    status is not work

--- js/app.js
+++ js/app.js
@@ -6,32 +6,56 @@
 var my_news = [
   {
     author: 'Гость 1',
-    text: 'Текст текст текст 1'
+    text: 'Небольшой текст 1',
+    bigText: 'Очень много текста 1 Очень много текста 1 Очень много текста 1'
   },
   {
     author: 'Гость 2',
-    text: 'Текст текст текст 2'
+    text: 'Небольшой текст 2',
+    bigText: 'Очень много текста 2 Очень много текста 2 Очень много текста 2 Очень много текста 2 Очень много текста 2'
   },
   {
     author: 'Гость 3',
-    text: 'Текст текст текст 3'
+    text: 'Небольшой текст 3',
+    bigText: 'Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3 Очень много текста 3'
   }
 ];
 
 class Article extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        visible: false,
+      };
+      this.handleClick = this.handleClick.bind(this);
+    };
+
+    handleClick() {
+      this.state = {
+          visible: true,
+      };
+    }
+
   render() {
     var author = this.props.latest_news.author,
-        text = this.props.latest_news.text;
+        text = this.props.latest_news.text,
+        bigText = this.props.latest_news.bigText,
+        visible = this.state.visible;
 
     return (
-      <div class = 'article'>
-        <p class = 'news__author'>{author}:</p>
-        <p class = 'news__text'>{text}</p>
+      <div className = 'article'>
+        <p className = 'news__author'>{author}:</p>
+        <p className = 'news__text'>{text}</p>
+
+        {/* для ссылки readmore: не показывай ссылку, если visible === true */}
+        <a href="#" onClick={this.handleClick} className={'news__readmore ' + (visible ? 'none': '')}>Подробнее</a>
+
+        {/* для большого текста: не показывай текст, если visible === false */}
+        <p className = {'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
       </div>
     )
   }
 }
-
 
 class News extends React.Component {
   render() {
@@ -69,7 +93,7 @@ class App extends React.Component {
     return (
       <div className = 'app'>
         <h3>Новости</h3>
-        <News  />  {/*в <> должно быть latest_news={my_news}*/}
+        <News latest_news={my_news} />  {/*в <> должно быть latest_news={my_news}*/}
       </div>
     );
   }
